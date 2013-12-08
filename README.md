653c85fdb3ecb2d676965d65022be2ea14b55dce
Joshua Wise <joshua@joshuawise.com>
2013-12-08 02:39:26 -0800

    Heavenly Ski Resort support

---
+++ README.md
@@ -0,0 +1,33 @@
+`snow`: a programmatic interface to snow reports
+================================================
+
+The `snow` package provides a programmatic interface for scraping snow
+reports.  As it turns out, OpenSnow isn't, and it seems that ski resorts
+don't provide APIs with all the data you might want.  A secondary (primary?)
+goal is to provide an excuse for me to learn BeautifulSoup...
+
+Currently, `snow` supports only Heavenly Ski Resort.  Other resorts may be
+implemented as time -- or pull requests -- permit.
+
+`snow` resort implementations should try to adhere to a standard-ish API --
+ideally, all modules should provide at least one function, `snow_report()`,
+which returns a dictionary kind of like the one that `heavenly` returns. 
+Features should be added in a way that will effectively generalize to other
+resorts, if possible.
+
+Currently, it's possible to do things like:
+
+    >>> r = heavenly.snow_report()
+    >>> print [run for run,details in r['runs'].items() if details['open']]
+    [u'Tamarack Return', u'California Tr.-Up', u"Orion's", u'California Trail', u'Big Dipper-Upper', u'Sledding', u'Big Dipper-Lower', u'Comet']
+    >>> print [lift for lift,details in r['lifts'].items() if details['open']]
+    [u'Gondola', u'Red Fir Mitey Mite', u'Tamarack Express', u'Dipper Express', u'Bear Cave Carpet', u'Comet Express', u'Big Easy']
+    >>> print r['stats']
+    {'runs': {'total': 97, 'open': 7}, 'percent': 7, 'acres': {'total': 4800, 'open': 44}}
+
+Other interesting wrapper utilities would be tracking tools to inform new
+lifts open and as new terrain opens, and provide appropriate messages;
+comparison tools to see which resorts got the most powder overnight; and
+`.login`-friendly tools that download and cache, and inform when the user
+should ragequit work and hit the slopes.
+
---
+++ heavenly.py
@@ -0,0 +1,91 @@
+import requests
+from bs4 import BeautifulSoup
+
+def snow_report():
+  r = requests.get("http://www.skiheavenly.com/the-mountain/snow-report/snow-report.aspx")
+  data = r.text
+  soup = BeautifulSoup(data)
+  
+  snow_24hr      = int(soup.find(attrs={"class": "newSnow"}).find_all("td")[0].get_text().split(" ")[0])
+  snow_overnight = int(soup.find(attrs={"class": "newSnow"}).find_all("td")[1].get_text().split(" ")[0])
+  snow_48hr      = int(soup.find(attrs={"class": "newSnow"}).find_all("td")[2].get_text().split(" ")[0])
+  snow_7day      = int(soup.find(attrs={"class": "newSnow"}).find_all("td")[3].get_text().split(" ")[0])
+  
+  snow_type         =     soup.find(attrs={"class": "snowConditions"}).find_all("th")[1].get_text()
+  snow_base         = int(soup.find(attrs={"class": "snowConditions"}).find_all("td")[0].get_text().split(" ")[0])
+  snow_season_total = int(soup.find(attrs={"class": "snowConditions"}).find_all("td")[1].get_text().split(" ")[0])
+  
+  r = requests.get("http://www.skiheavenly.com/the-mountain/terrain-and-lift-status.aspx")
+  data = r.text
+  soup = BeautifulSoup(data)
+  
+  lifts_open  = int(soup.find(id="columnRight_ctl00_liLiftsOpen").find_all("span")[0].get_text())
+  lifts_total = int(soup.find(id="columnRight_ctl00_liLiftsOpen").find_all("span")[1].get_text())
+  
+  runs_open  = int(soup.find(id="columnRight_ctl00_liRunsOpen").find_all("span")[0].get_text())
+  runs_total = int(soup.find(id="columnRight_ctl00_liRunsOpen").find_all("span")[1].get_text())
+  
+  acres_open  = int(soup.find(id="columnRight_ctl00_liAcresOpen").find_all("span")[0].get_text())
+  acres_total = int(soup.find(id="columnRight_ctl00_liAcresOpen").find_all("span")[1].get_text())
+  
+  percent_open = int(soup.find(id="columnRight_ctl00_liOpenPercent").find_all("span")[0].get_text().split("%")[0])
+  
+  # Okay, here comes the fun bit.
+  regions = soup.find(id="ScheduledRuns").find_all("div", recursive=False)
+  
+  def text_to_status(s):
+    if s == 'Yes': return True
+    if s == 'No': return False
+    return s
+  
+  runs = \
+    { run.find_all("td")[1].get_text(): \
+      { "name": run.find_all("td")[1].get_text(), \
+        "difficulty": run.find_all("img")[0]['alt'], \
+        "open": text_to_status(run.find_all("img")[1]['alt']), \
+        "region": r.find_all("h2")[0].get_text().split(" - ")[0] \
+      } \
+      for r in regions \
+      for tab in r.find("tr").find_all("table") \
+      for run in tab.find_all("tr")[1:] \
+    }
+  
+  lifts = \
+    {lift.find_all("td")[0].get_text().strip(): \
+      { "type": lift.find_all("td")[1].get_text().strip(), \
+        "open": text_to_status(lift.find_all("td")[2].find("img")['alt']) \
+      } \
+      for lift in soup.find(id="Lifts").find("table").find_all("tr")[1:] \
+    }
+  
+  words = soup.find(attrs={"class": "introText"}).get_text()
+  
+  # From http://www.snow.com/faqdetail/Snow-Stake-Cams/Where-do-you-measure.axd:
+  #   The official snow stake at Heavenly is located near Dipper Express at
+  #   9,800 feet.  Two additional snow stakes are the Mott Canyon snow stake,
+  #   located at 8,900 feet and the Sky Deck snow stake, located at 8,550
+  #   feet.  The snow stake at Dipper Express is our official snow stake
+  #   camera and the one we use to record our historical snowfall totals.
+  
+  return { \
+    "description": words, \
+    "snow_stakes": { \
+      "dipper_express": { \
+        "elevation": 9800, \
+        "overnight": snow_overnight, \
+        "hours_24": snow_24hr, \
+        "hours_48": snow_48hr, \
+        "days_7": snow_7day, \
+        "conditions": snow_type, \
+        "base": snow_base, \
+        "season_total": snow_season_total \
+      }, \
+    }, \
+    "lifts": lifts, \
+    "runs": runs, \
+    "stats": { \
+      "runs": {"open": runs_open, "total": runs_total}, \
+      "acres": {"open": acres_open, "total": acres_total}, \
+      "percent": percent_open \
+    } \
+  }
